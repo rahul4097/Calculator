2fa50aa5d64925877ef0af12237ed3cdcec4f14e
rahul4097 <138388064+rahul4097@users.noreply.github.com>
2023-07-19 23:12:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let string = "";
+let buttons= document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e) =>{
+    if(e.target.innerHTML == '='){
+      string=eval(string);
+      document.querySelector('input').value=string;
+    }else if(e.target.innerHTML == 'AC'){
+      string=""
+      document.querySelector('input').value=string;
+    }else if(e.target.innerHTML == 'del'){
+      string= string.substring(0,string.length-1);
+      document.querySelector('input').value=string;
+    }else if(e.target.innerHTML == '%'){
+        string=string/100;
+        document.querySelector('input').value=string;
+     }else{
+  console.log(e.target);
+    string+= e.target.innerHTML;
+    document.querySelector('input').value=string;
+    }
+  })
+})
